783dc6e52279bc75707ebb5267fe669c4336e6ba
Tako-San <dxe13@mail.ru>
2020-09-13 12:09:34 +0300

    request method algorithm written in pseudo code, some code in cache struct written

--- cache/cache.h
+++ cache/cache.h
@@ -5,31 +5,104 @@
 #include <unordered_map>
 #include <iostream>
 
+// TODO: подумать нужно ли делать тип ключа шаблонным
 template <typename T, typename KeyT = int>
-struct cache_t
+struct Cache_t
 {
-    size_t size_;
-    std::list<T> cache_;
+    size_t cap;
+    std::list<T> cache;
 
-    struct bucket
+    using lst_it = typename std::list<T>::iterator;
+
+    struct bucket_el
     {
-        typename std::list<T>::iterator lst_it;
+        lst_it data;
         size_t call_num;
     };
 
-    std::unordered_map<KeyT, bucket> hash_table;
+    using u_map = typename std::unordered_map<KeyT, std::list<bucket_el>>;
+    using u_map_it = typename u_map::iterator;
 
-    cache_t( size_t size_ ) : size_(size_)
+    u_map hash_tbl;
+
+    Cache_t( size_t cap ) : cap(cap),
+                            cache(cap)
     {}
 
-    ~cache_t( )
+    ~Cache_t( )
     {}
 
-    bool lookup( const T * elem)
+    T & request( const T & data )
     {
+        KeyT key = hash(data) % cap;
+        auto search = hash_tbl.find(key);
 
+        if (search != hash_tbl.end())
+        {
+            //создаем новый элемент мапа
+            //  put_in_cache();
+        }
+        else
+        {
+            //сравниваем данные в листе элементов бакетов с входными
+            //  если нашли совпадение:
+            //    возвращаем итератор на найденный элемент листа
+            //    КОНЕЦ
+            //
+            //  если не нашли:
+            //    put_in_cache();
+        }
+    }
+
+    KeyT hash( const T & data)
+    {
+        return data;
     }
 };
+
+/*template <typename T, typename KeyT = int>
+struct cache_t
+{
+    size_t sz_;
+    std::list<T> cache_;
+
+    typedef typename std::list<T>::iterator ListIt;
+    std::unordered_map<int, ListIt> hash_;
+
+    cache_t(size_t sz) : sz_(sz) {}
+
+    bool full() const;
+    bool lookup(const T *elem)
+    {
+        auto hit = hash_.find(elem->id);
+        if (hit == hash_.end())
+        {
+            if (full())
+            {
+                hash_.erase(cache_.back().id);
+                cache_.pop_back();
+            }
+            cache_.push_front(*elem);
+            hash_[elem->id] = cache_.begin();
+            return false;
+        }
+
+        auto eltit = hit->second;
+
+        if (eltit != cache_.begin())
+            cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
+
+        return true;
+    }
+};
+
+struct page_t
+{
+    int id;   // page index: 1, 2, ... n
+    int sz;      // page size
+    char *data;  // page data
+};*/
+
 
 
 #endif //ILAB2_CACHE_H
