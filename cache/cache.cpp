75ddb35bfd194a5e72e3b02075896768f358157b
Tako-San <dxe13@mail.ru>
2020-09-14 17:42:15 +0300

    It's alive! But still disgusting

--- cache/cache.cpp
+++ cache/cache.cpp
@@ -1,4 +1,23 @@
-//
-// Created by farid on 14.09.2020.
-//
+#include "cache.h"
 
+int main()
+{
+    using lst_it = typename std::list<int>::iterator;
+    Cache_t<int, int> c(4);
+
+    c.put_in_cache(1);
+    c.put_in_cache(2);
+    c.put_in_cache(3);
+    c.put_in_cache(4);
+
+    for( const auto& n : c.cache )
+    {
+        std::cout << "Value: " << n << "\n";
+    }
+
+    lst_it x = c.request(2);
+
+    std::cout << *x << std::endl;
+
+    return 0;
+}
--- cache/cache.h
+++ cache/cache.h
@@ -1,5 +1,5 @@
-#ifndef ILAB2_CACHE_V1_5_H
-#define ILAB2_CACHE_V1_5_H
+#ifndef ILAB2_CACHE_H
+#define ILAB2_CACHE_H
 
 #include <list>
 #include <unordered_map>
@@ -23,17 +23,46 @@ struct Cache_t
     u_map hash_tbl;
 
     Cache_t( size_t cap ) : cap(cap),
-                            cache(cap)
+                            cache(0)
     {}
 
     ~Cache_t( )
     {}
 
-    lst_it request()
+    lst_it request(KeyT key)
     {
+        auto search_res = hash_tbl.find(key);
 
+        if (search_res != hash_tbl.end())
+        {
+            (*search_res).second.call_num++;
+            return (*search_res).second.link;
+        }
+        else
+        {
+            lst_it res = put_in_cache(key);
+
+            page_info tmp = {res, 1};
+
+            hash_tbl[key] = tmp;
+
+            return res;
+        }
+
+    }
+
+    lst_it put_in_cache(KeyT key)
+    {
+        T tmp = load_from_web(key);
+        cache.push_back(tmp);
+        return --cache.end();
+    }
+
+    T load_from_web(KeyT key)
+    {
+        return key;
     }
 
 };
 
-#endif //ILAB2_CACHE_V1_5_H
+#endif //ILAB2_CACHE_H
