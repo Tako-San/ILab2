6362334a75a90e652aebcaf53013c05d3a8305df
Tako-San <dxe13@mail.ru>
2020-09-14 21:43:33 +0300

    main for testing added

--- cache/cache.cpp
+++ cache/cache.cpp
@@ -2,22 +2,59 @@
 
 int main()
 {
-    using lst_it = typename std::list<int>::iterator;
-    Cache_t<int, int> c(4);
+    // using lst_it = typename std::list<int>::iterator;
+    /*Cache_t<int, int> c(4);
 
-    c.put_in_cache(1);
-    c.put_in_cache(2);
-    c.put_in_cache(3);
-    c.put_in_cache(4);
+    c.request(1); // 1
+    c.request(2); // 1
+    c.request(3); // 1
+    c.request(4); // 1
+    c.request(5); // 1
 
-    for( const auto& n : c.cache )
-    {
+
+    c.request(1); // 2
+    c.request(1); // 3
+    c.request(2); // 2
+    c.request(2); // 3
+    c.request(5); // 2
+    c.request(5); // 3
+
+    c.request(6);
+
+    for(const auto& n : c.cache)
         std::cout << "Value: " << n << "\n";
+
+    auto x = c.request(2);
+
+    std::cout << x << std::endl;
+
+    std::cout << c << std::endl;*/
+
+    int hits = 0, capacity = 0, calls_amount = 0;
+
+    std::cout << "Input maximum capacity of cache:\n";
+
+    std::cin >> capacity;
+
+    std::cout << "Input amount of calls:\n";
+
+    std::cin >> calls_amount;
+
+    Cache_t<int> lfu(capacity);
+
+    for (int i = 0; i < calls_amount; ++i)
+    {
+        int request = 0;
+        std::cout << "Input request #" << i << "\n";
+        std::cin >> request;
+
+        lfu.request(request);
     }
 
-    lst_it x = c.request(2);
+    std::cout << "Total amounts of hits = " << hits << "\n";
+    std::cout << "------------LIST DUMP-----------------------\n";
+    std::cout << lfu << "\n";
 
-    std::cout << *x << std::endl;
 
     return 0;
 }
