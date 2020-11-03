fb1a530a68674e79ee540d60e1b3f0ca63592d51
Tako-San <dxe13@mail.ru>
2020-11-03 22:24:35 +0300

    Some progress

--- 4_Matrix/matrix.h
+++ 4_Matrix/matrix.h
@@ -1,6 +1,10 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <initializer_list>
+
+using std::initializer_list;
+
 template <typename DataT>
 class Matrix final
 {
@@ -9,6 +13,8 @@ private:
 
     int rows_, cols_;
 
+    bool is_inv{false};
+
 public:
 
     Matrix( int rows, int cols ) : rows_{rows}, cols_{cols}
@@ -16,9 +22,31 @@ public:
         memory_allocation(rows_, cols_);
     }
 
+    Matrix( int rows, int cols, const initializer_list<DataT> & dat ) : rows_{rows}, cols_{cols}
+    {
+        memory_allocation(rows_, cols_);
+
+        int max = rows_ * cols_;
+        int idx = 0;
+
+        for (auto cur = dat.begin(), end = dat.end(); cur != end & idx < max; ++cur, ++idx )
+        {
+            int row = idx / cols_,
+                col = idx % cols_;
+            data_[row][col] = *cur;
+        }
+    }
+
     Matrix( const Matrix<DataT> & orig ) : rows_{orig.rows_}, cols_{orig.cols_}
     {
         memory_allocation(rows_, cols_);
+
+        for (int i = 0, max = cols_ * rows_; i < max; ++i)
+        {
+            int col = i / cols_,
+                row = i % cols_;
+            data_[row][col] = orig.data_[row][col];
+        }
     }
 
     ~Matrix( )
@@ -26,13 +54,10 @@ public:
         memory_free();
     }
     
-    Matrix<DataT> & operator += ( const Matrix<DataT> & m)
+    Matrix<DataT> & operator += ( const Matrix<DataT> & m )
     {
-        if (cols_ != m.cols_ || rows_ != m.rows_)
-        {
+        if (!size_suitable(m))
             memory_free();
-            cols_ = rows_ = -1;
-        }
 
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < cols_; ++j)
@@ -41,13 +66,10 @@ public:
         return *this;
     }
 
-    Matrix<DataT> & operator -= ( const Matrix<DataT> & m)
+    Matrix<DataT> & operator -= ( const Matrix<DataT> & m )
     {
-        if (cols_ != m.cols_ || rows_ != m.rows_)
-        {
+        if (!size_suitable(m))
             memory_free();
-            cols_ = rows_ = -1;
-        }
 
         for (int i = 0; i < rows_; ++i)
             for (int j = 0; j < cols_; ++j)
@@ -56,8 +78,28 @@ public:
         return *this;
     }
 
+    Matrix<DataT> & operator *= ( double mul )
+    {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
+                data_[i][j] *= mul;
+
+        return *this;
+    }
+
+    DataT * operator [] ( int idx )
+    {
+        return data_[idx];
+    }
+
+    bool size_suitable( const Matrix<DataT> & matr ) const
+    {
+        return cols_ == matr.cols_ && rows_ == matr.rows_ && !is_inv && !matr.is_inv;
+    }
+
+
 private:
-    
+
     void memory_allocation( int rows, int cols )
     {
         data_ = new DataT * [rows];
@@ -72,6 +114,9 @@ private:
             delete [] data_[i];
 
         delete [] data_;
+
+        cols_ = rows_ = -1;
+        is_inv = true;
     }
     
     void memory_resize( int rows, int cols )
@@ -94,6 +139,18 @@ template<typename DataT>
 Matrix<DataT> operator - ( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
 {
     return (Matrix<DataT>{lhs} -= rhs);
+}
+
+template<typename DataT>
+Matrix<DataT> operator * ( const Matrix<DataT> & matr, double mul )
+{
+    return (Matrix<DataT>{matr} *= mul);
+}
+
+template<typename DataT>
+Matrix<DataT> operator * ( double mul, const Matrix<DataT> & matr )
+{
+    return (Matrix<DataT>{matr} *= mul);
 }
 
 #endif //MATRIX_H
--- 4_Matrix/test.cpp
+++ 4_Matrix/test.cpp
@@ -7,9 +7,66 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-TEST(constructor, tst1)
+TEST(ctor, init_lst)
 {
-    Matrix<int> m{2, 2};
+    Matrix<int> m{2, 2, {1, 2, 3, 4}};
+
+    EXPECT_EQ(m[0][0], 1);
+    EXPECT_EQ(m[0][1], 2);
+    EXPECT_EQ(m[1][0], 3);
+    EXPECT_EQ(m[1][1], 4);
+}
+
+TEST(ctor, copy)
+{
+    Matrix<int> m1{2, 2, {1, 2, 3, 4}};
+    Matrix<int> m2{m1};
+
+    EXPECT_EQ(m2[0][0], 1);
+    EXPECT_EQ(m2[0][1], 2);
+    EXPECT_EQ(m2[1][0], 3);
+    EXPECT_EQ(m2[1][1], 4);
+}
+
+TEST(op, mul)
+{
+    Matrix<int> m1{2, 2, {1, 0, 0, 1}};
+    m1 *= 2;
+
+    EXPECT_EQ(m1[0][0], 2);
+    EXPECT_EQ(m1[0][1], 0);
+    EXPECT_EQ(m1[1][0], 0);
+    EXPECT_EQ(m1[1][1], 2);
+
+    Matrix<int> m2{m1 * 2};
+
+    EXPECT_EQ(m2[0][0], 4);
+    EXPECT_EQ(m2[0][1], 0);
+    EXPECT_EQ(m2[1][0], 0);
+    EXPECT_EQ(m2[1][1], 4);
+
+    Matrix<int> m3{2 * m1};
+
+    EXPECT_EQ(m3[0][0], 4);
+    EXPECT_EQ(m3[0][1], 0);
+    EXPECT_EQ(m3[1][0], 0);
+    EXPECT_EQ(m3[1][1], 4);
+}
+
+TEST(op, plus_minus)
+{
+    Matrix<int> m1{2, 2, {0, 1, 2, 3}};
+    Matrix<int> m2{m1};
+
+    Matrix<int> sum = m1 + m2;
+
+    for (int i = 0; i < 4; ++i)
+        EXPECT_EQ(sum[i / 2][i % 2], 2 * i);
+
+    Matrix<int> diff = m1 - m2;
+
+    for (int i = 0; i < 4; ++i)
+        EXPECT_EQ(diff[i / 2][i % 2], 0);
 }
 
 int main( int argc, char ** argv )
