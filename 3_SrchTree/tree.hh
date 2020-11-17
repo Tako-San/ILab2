3004f2685f18cc52fe922ff5bb50f591837f5441
Tako-San <dxe13@mail.ru>
2020-11-18 01:30:16 +0300

    tree.hh -> tree.hh + tree.ii

--- 3_SrchTree/tree.hh
+++ 3_SrchTree/tree.hh
@@ -22,7 +22,8 @@ namespace F
     private:
 
         NodeT * root_;
-        bool is_empty{true};
+        bool is_empty_{true};
+        int el_num_{0};
 
     public:
 
@@ -58,102 +59,7 @@ namespace F
         void print_lvl( int lvl );
     };
 
-
-
-
-    template <typename DataT>
-    Tree<DataT>::Tree( ) : root_{new NodeT}
-    {}
-
-    template <typename DataT>
-    Tree<DataT>::Tree( const DataT & data ) : root_{new NodeT{data}},
-                                              is_empty{false}
-    {}
-
-    template <typename DataT>
-    Tree<DataT>::~Tree( )
-    {
-        root_->clear_subtree();
-        is_empty = true;
-    }
-
-    template <typename DataT>
-    template <typename FindT>
-    NodeIt<DataT> Tree<DataT>::find( const FindT & data )
-    {
-        return NodeIt<DataT> {root_->find(data)};
-    }
-
-    template <typename DataT>
-    template <typename FindT>
-    NodeIt<DataT> Tree<DataT>::lower_bound( const FindT & data )
-    {
-        return NodeIt<DataT> {root_->lower_bound(data)};
-    }
-
-    template <typename DataT>
-    NodeIt<DataT> Tree<DataT>::begin( )
-    {
-        return NodeIt<DataT> {root_->findmin()};
-    }
-
-    template <typename DataT>
-    NodeIt<DataT> Tree<DataT>::end( )
-    {
-        return NodeIt<DataT> {nullptr};
-    }
-
-    template <typename DataT>
-    void Tree<DataT>::insert( const DataT & data )
-    {
-        if (is_empty)
-        {
-            root_->data_ = data;
-            is_empty = false;
-        }
-        else
-            root_ = root_->insert(data);
-    }
-
-    template <typename DataT>
-    int Tree<DataT>::depth( )
-    {
-        return root_->depth();
-    }
-
-    template <typename DataT>
-    bool Tree<DataT>::is_balanced( )
-    {
-        return root_->is_balanced();
-    }
-
-    template <typename DataT>
-    void Tree<DataT>::remove( DataT & to_del )
-    {
-        root_ = root_->remove(to_del);
-    }
-
-    template <typename DataT>
-    void Tree<DataT>::print( )
-    {
-        root_->print();
-    }
-
-    template <typename DataT>
-    void Tree<DataT>::print_leafs( )
-    {
-        if(root_ == nullptr)
-            return;
-        root_->print_leafs();
-    }
-
-    template <typename DataT>
-    void Tree<DataT>::print_lvl( int lvl )
-    {
-        if(root_ == nullptr)
-            return;
-        root_->print_lvl(lvl);
-    }
+    #include "tree.ii"
 }
 
 #endif //TREE_HH
