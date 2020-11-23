12ec8578e5a0afce12730aa7a074c6d7c4e199a5
Tako-San <dxe13@mail.ru>
2020-11-23 16:40:10 +0300

    README.md updated + some progress

--- 4_Matrix/main.cc
+++ 4_Matrix/main.cc
@@ -15,15 +15,19 @@ int main( )
     uint size = 0;
     cin >> size;
 
+    uint el_num = size * size;
+
     vector<double> raw{};
-    for (int i = 0, end = size * size; i < end; ++i)
+    raw.reserve(el_num);
+
+    for (int i = 0, end = el_num; i < end; ++i)
     {
         double tmp;
         cin >> tmp;
         raw.push_back(tmp);
     }
 
-    Matrix<double> matr{size, size, raw};
+    Matrix<double> matr{size, size, raw.begin(), raw.end()};
 
     cout << matr.det() << endl;
 
